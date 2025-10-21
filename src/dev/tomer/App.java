6f765f9da754fbf6dadd70e2c5c5fb580337abbe
Yoram Vardi <vardi.tomer@gmail.com>
2025-10-20 17:42:25 -0700

    Initial commit

---
+++ src/dev/tomer/App.java
@@ -0,0 +1,17 @@
+package dev.tomer;
+
+import javax.swing.*;
+
+public class App {
+
+    public static void init(){
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ src/dev/tomer/Entity/Entity.java
@@ -0,0 +1,4 @@
+package dev.tomer.Entity;
+
+public class Entity {
+}
